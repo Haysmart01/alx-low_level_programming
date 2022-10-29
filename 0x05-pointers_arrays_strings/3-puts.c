ef8ccb35e5edc7f4a77b873f308b6351e1f0c1b7
Haysmart01 <mikehaysmart@gmail.com>
2022-10-29 00:04:53 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,14 +1,12 @@
 #include "main.h"
 
 /**
- * _puts - prints a string
- * @str: string to be printed
+ * _puts - Prints a string
+ *  @str: string.
  */
 void _puts(char *str)
 {
 	while (*str)
-	{
 		_putchar(*str++);
-	}
 	_putchar('\n');
 }
